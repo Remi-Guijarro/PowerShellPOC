0cc0ccf84999a0158828dc51b511b2dd78522644
Remi-Guijarro <remi.guijarro@outlook.fr>
2021-09-22 14:38:39 +0200

    tested user input and output powershell style

--- test.ps1
+++ test.ps1
@@ -1,1 +1,11 @@
 Write-Host("Hello " + $env:USERNAME)
+
+function Receive-Output {
+    process { write-host $_ -ForegroundColor Red}
+}
+
+write-output coucou | Receive-Output
+
+$test =  Read-Host "Who are you ?" -AsSecureString
+
+Write-Output "you said you're $test, that's right ?" | Receive-Output 
